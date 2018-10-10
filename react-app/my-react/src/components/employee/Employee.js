348455b75b556b877cbea43ad3bb7c890f3bf601
Karthick <karthick236@gmail.com>
2018-10-10 17:12:53 +0530

    changes

--- react-app/my-react/src/components/employee/Employee.js
+++ react-app/my-react/src/components/employee/Employee.js
@@ -1,7 +1,46 @@
 import React from "react";
 class Employee extends React.Component {
   render() {
-    return <h3>Employee</h3>;
+    return (
+      <section class="content">
+        <h1>Employee</h1>
+        <form action="">
+          <div>
+            <input type="button" value="Create" />
+            <input type="button" value="Update" />
+            <input type="button" value="Delete" />
+          </div>
+          <div>
+            <table id="tab">
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Name</th>
+                  <th>&nbsp;</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr>
+                  <td>100</td>
+                  <td>Human Resources</td>
+                  <td>
+                    <input name="dept" type="radio" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>100</td>
+                  <td>Human Resources</td>
+                  <td>
+                    <input name="dept" type="radio" />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </form>
+      </section>
+    );
   }
 }
 
--- react-app/my-react/src/components/login/Login.js
+++ react-app/my-react/src/components/login/Login.js
@@ -2,7 +2,27 @@ import React from "react";
 
 class Login extends React.Component {
   render() {
-    return <h1>Login</h1>;
+    return (
+      <section class="content">
+        <h1>Login</h1>
+        <form action="">
+          <div class="form-input">
+            <label>Username:</label>
+            <input type="text" />
+          </div>
+
+          <div class="form-input">
+            <label>Password:</label>
+            <input type="password" />
+          </div>
+
+          <div class="form-input">
+            <input type="button" value="Login" />
+            <input type="button" value="Reset" />
+          </div>
+        </form>
+      </section>
+    );
   }
 }
 
